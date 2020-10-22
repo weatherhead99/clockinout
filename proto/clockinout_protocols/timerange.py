0a172b938d4762f18f36103ecb4ae596294056b9
Dan <dan.weatherill@cantab.net>
2020-10-22 04:36:06 +0100

    more time utility functions

--- proto/clockinout_protocols/timerange.py
+++ proto/clockinout_protocols/timerange.py
@@ -7,21 +7,30 @@ Created on Thu Oct 22 04:19:12 2020
 """
 
 from math import modf
-from datetime import datetime
+from datetime import datetime, timedelta
 from google.protobuf.timestamp_pb2 import Timestamp
 from .clockinoutservice_pb2 import TimeRange
+from typing import Tuple
 
-def to_google_timestamp(time: datetime):
+def to_google_timestamp(time: datetime) -> Timestamp:
     unix_time = time.timestamp()
     frac, integer = modf(unix_time)
     nanos = int(frac * 1E9)
     seconds = int(integer)
     return Timestamp(seconds=seconds, nanos=nanos)
 
-def to_timerange(start: datetime, end: datetime):
+def to_timerange(start: datetime, end: datetime) -> TimeRange:
     startts = to_google_timestamp(start)
     endts = to_google_timestamp(end)
     return TimeRange(start=startts, end=endts)
 
+#NOTE: discards some accuracy since python datetime doesn't support nanoseconds
+def from_google_timestamp(ts: Timestamp) -> datetime:
+    micros = int(round(ts.nanos / 1E3))
+    dt = datetime.fromtimestamp(ts.seconds)
+    dt += timedelta(microseconds=micros)
+    return dt
 
+def from_timerange(tr: TimeRange) -> Tuple[datetime, datetime]:
+    return from_google_timestamp(tr.start), from_google_timestamp(tr.end)
 
