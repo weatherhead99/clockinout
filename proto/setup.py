7553695f6ee8186799261263851d7ecd3bca027a
Dan <dan.weatherill@cantab.net>
2020-10-22 03:27:43 +0100

    fix proto build system (I think)

--- proto/setup.py
+++ proto/setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup, find_packages
 from setuptools.command.build_py import build_py
+from setuptools.command.develop import develop
+from setuptools.command.install import install
 from grpc_tools.command import BuildPackageProtos
 
-
-class CustomBuild(build_py):
+class RunProtoBuildBefore:
     def run(self):
         self.run_command("build_package_protos")
-        build_py.run(self)
+        return super().run()
 
 
+class CustomDevelop(RunProtoBuildBefore, develop):...
+class CustomInstall(RunProtoBuildBefore, install):...
+class CustomBuild(RunProtoBuildBefore, build_py):...
 
 setup(
       name="clockinout_protocols",
@@ -16,7 +20,10 @@ setup(
       packages = find_packages(),
       package_dir = {"" : "."},
       cmdclass = {
+          "build_package_protos" : BuildPackageProtos,
           "build_py" : CustomBuild,
-          "build_package_protos" : BuildPackageProtos},
+          "develop" : CustomDevelop,
+          "install" : CustomInstall
+          },
       include_package_data=True)
 
