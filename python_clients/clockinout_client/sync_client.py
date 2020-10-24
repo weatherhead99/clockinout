783ba319aa591d0d1a04aa32ec80cf62e959a0fc
Dan <dan.weatherill@cantab.net>
2020-10-24 19:12:53 +0100

    start on sync API client properly

--- python_clients/clockinout_client/sync_client.py
+++ python_clients/clockinout_client/sync_client.py
@@ -10,23 +10,37 @@ import grpc
 from clockinout_protocols.errors import check_response_errors
 from clockinout_protocols import clockinoutservice_pb2
 from clockinout_protocols.clockinoutservice_pb2_grpc import ClockInOutServiceStub
+from clockinout_protocols.clockinout_queries_pb2_grpc import ClockInOutQueryServiceStub
+from clockinout_protocols.clockinout_management_pb2_grpc import ClockInOutManagementServiceStub
 from collections import namedtuple
-from typing import Union, Iterable
+from typing import Union, Iterable, TypeVar, Callable, Any
+from google.protobuf.message import Message
+from types import MethodType
 
-serverinfo = namedtuple("serverinfo", ["version", "proto_version","public_key",
-                                       "legacy_enabled"])
-userinfo = namedtuple("userinfo", ["id", "name"])
+from functools import wraps, partial
+
+Mess = TypeVar("Message", bound=Message)
+#todo: work out what type of context should be
+StubMeth = Callable[[Mess, Any], Mess]
 
 class ClockinoutSyncClient:
+
     def __init__(self, connstr: str, **callkwargs):
         self.channel = grpc.insecure_channel(connstr)
-        self.stub = ClockInOutServiceStub(self.channel)
+        
+        self.stubs = [ClockInOutServiceStub(self.channel),
+                      ClockInOutManagementServiceStub(self.channel),
+                      ClockInOutQueryServiceStub(self.channel)]
         self.callkwargs = callkwargs
         
-    @property 
-    def ServerInfo(self):
-        req = clockinoutservice_pb2.empty()
-        resp = self.stub.GetServerInfo(req, **self.callkwargs)
-        return serverinfo(resp.version, resp.proto_version, resp.tag_provision_,
-                          resp.legacy_enabled)
+        for stub in self.stubs:
+            for methname, methcallable in stub.__dict__.items():
+                if isinstance(methcallable, grpc._channel._UnaryUnaryMultiCallable):
+                    #note HACK to get round python closure late-binding
+                    def f(instance, req, __stubfun=getattr(stub, methname)):
+                        return instance.call_unary_rpc(req, __stubfun)
+                    setattr(self, methname, MethodType(f,self))
+
+    def call_unary_rpc(self, req, stubmethod):
+        return check_response_errors(stubmethod(req, **self.callkwargs))
 
--- python_clients/devclient1.py
+++ python_clients/devclient1.py
@@ -8,29 +8,20 @@ Created on Wed Oct 21 22:40:25 2020
 
 from clockinout_protocols.errors import check_response_errors
 from clockinout_protocols.clockinoutservice_pb2 import empty, UserInfo
-from clockinout_protocols.clockinoutservice_pb2_grpc import ClockInOutServiceStub
 
-from clockinout_protocols.clockinout_management_pb2_grpc import ClockInOutManagementServiceStub
 from clockinout_protocols.clockinout_management_pb2 import ItemRequest
+
+from clockinout_protocols.clockinout_queries_pb2 import UserQueryRequest, OrgQueryRequest
 
 import grpc
 from datetime import datetime
 
+from clockinout_client.sync_client import ClockinoutSyncClient
 
-channel = grpc.insecure_channel("localhost:50051")
-stub = ClockInOutServiceStub(channel)
-manstub = ClockInOutManagementServiceStub(channel)
+client = ClockinoutSyncClient("localhost:50051")
 
-req = empty()
-server_info = stub.GetServerInfo(req)
+server_info = client.call_unary_rpc(empty(), client.stubs[0].GetServerInfo)
 
-print(server_info)
+sinfo3 = client.GetServerInfo(empty())
 
-#admin_login
-ulogin = UserInfo(name="admin", password="Passw0rd99")
-login_response = check_response_errors(stub.AdminLogin(ulogin))
-
-
-req = ItemRequest(user=UserInfo(name="new_user_name2"))
-add_response = check_response_errors(manstub.NewItem(req))
-
+login_response = client.AdminLogin(UserInfo(name="admin", password="Passw0rd99"))
